c2a4ecb67c4d10901d030c69d6a9edd2d3bbaa44
Tomás F. Vilalta <84082278+TomasVilalta@users.noreply.github.com>
2025-03-14 11:25:26 -0300

    feat: add tailwindcss-animated package and update styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Thread } from "@/components/thread";
 import Image from "next/image";
 import {
@@ -10,15 +12,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <div className="h-screen w-screen">
+    <div className="h-screen relative">
+      {/* prevent scroll */}
+      <div className="h-full overflow-hidden">
         <Thread />
       </div>
-      <div className="absolute sm:top-4 sm:right-8 sm:w-12 top-4 right-4">
+      <div className="absolute sm:top-4 sm:right-8 sm:w-12 w-8 top-4 right-4">
         {/* a small button on the bottom right corner on desktop and on the top right corner on mobile */}
         <TooltipProvider>
           <Tooltip>
-            <TooltipTrigger>
+            <TooltipTrigger className="animate-jump-in animate-duration-200 animate-delay-800">
               <Link
                 href="https://github.com/TomasVilalta/promtior-challenge"
                 target="_blank"
@@ -30,6 +33,7 @@ export default function Home() {
                   width={120}
                   height={120}
                   className="rounded-full"
+                  priority
                 />
               </Link>
             </TooltipTrigger>
@@ -39,6 +43,6 @@ export default function Home() {
           </Tooltip>
         </TooltipProvider>
       </div>
-    </>
+    </div>
   );
 }
--- components/WelcomeScreen.tsx
+++ components/WelcomeScreen.tsx
@@ -6,11 +6,13 @@ import Link from "next/link";
 export default function WelcomeScreen() {
   return (
     <>
-      <PromtiorLogo className="w-16 h-16 bg-transparent logo " />
-      <p className="mt-4 font-medium">Welcome to the Promtior Assistant!</p>
+      <PromtiorLogo className="w-16 z-10 h-16 bg-transparent logo animate-fade-up animate-duration-600" />
+      <p className="mt-4 font-medium animate-fade-down animate-duration-500 animate-delay-100">
+        Welcome to the Promtior Assistant!
+      </p>
       <Button
         variant="link"
-        className="text-muted-foreground cursor-pointer hover:text-cyan-500"
+        className="text-muted-foreground cursor-pointer hover:text-cyan-500 animate-fade-down animate-duration-400 animate-delay-200"
       >
         <Link href="https://www.promtior.ai" target="_blank">
           www.promtior.ai
